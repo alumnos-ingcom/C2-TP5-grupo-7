ed05484347ceb9e20176039e7e2a1cd33da87bdc
hernangp <hernang.palumbo@gmail.com>
2021-06-15 18:23:38 -0300

    Correcion 1

--- tp5ej3.py
+++ tp5ej3.py
@@ -3,11 +3,19 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
+def pedir_numero():    
+    while True:
+        try:
+            numero=int(input('¿Que valor de la serie de Tribonacci queres ver? #'))
+            if numero>0:
+                return numero
+            else:
+                print("Eres un tonto!!!")
+        except ValueError:
+            print("Eres un tonto!!!")
+
 def tribonacci(cantidad_terminos):
     """La funcion devuelve una lista con la cantidad de terminos de la serie de tribonacci deseados"""
-    i=1
-    j=1
-    k=1
     vuelta = 1
     terminos = []
     while (cantidad_terminos-vuelta)>=0:
@@ -15,19 +23,17 @@ def tribonacci(cantidad_terminos):
             terminos.append(1)
             vuelta = vuelta + 1
         else:
-            l = i + j + k
-            terminos.append(l)
-            i = j
-            j = k
-            k = l
+            nuevo_termino = terminos[-1]+terminos[-2]+terminos[-3]
+            terminos.append(nuevo_termino)
             vuelta = vuelta +1
-    return terminos
+    return terminos[-1]
             
 
-def prueba(terminos):
-    terminos = terminos
-    print(f'los {terminos} primero términos de la serie de tribonacci son: ', tribonacci(10))
+def prueba():
+    termino = pedir_numero()
+    valor_termino = tribonacci(termino)
+    print(f'el valor del termino {termino} de la serie de tribonacci es: ', valor_termino )
 
 if __name__ == "__main__":
-    prueba(10)
+    prueba()
 
