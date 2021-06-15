a01d2015b672563bf34cff356ffc0210e45492b7
hernangp <hernang.palumbo@gmail.com>
2021-06-15 18:36:24 -0300

    Correccion 1

--- tp5ej4.py
+++ tp5ej4.py
@@ -13,23 +13,21 @@ def es_perfecto(numero):
             if numero % divisores == 0:
                 suma_divisores += divisores
             divisores -= 1
-        if suma_divisores == numero:
-            return True
-        else: 
-            return False
+        return suma_divisores == numero
     else:
         raise IngresoIncorrecto('El numero ingresado no es un entero positivo')
 
-def prueba(numero):
+def prueba():
+    numero_a_verficar = int(input('ingrese un valor para verificar si es perfecto: #'))
     """Toda la interacción con el usuario va acá"""
     try:
-        if es_perfecto(numero):
-            print(f'{numero} es perfecto')
+        if es_perfecto(numero_a_verficar):
+            print(f'{numero_a_verficar} es perfecto')
         else:
-            print(f'{numero} no es perfecto')
+            print(f'{numero_a_verficar} no es perfecto')
     except IngresoIncorrecto as err:
         print(err)
 
 if __name__ == "__main__":
-    prueba(8128)
+    prueba()
 
