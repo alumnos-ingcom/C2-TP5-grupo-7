06f19ff41ed7d1df7a6904115b5020b407cf83b4
hernangp <hernang.palumbo@gmail.com>
2021-06-15 19:22:22 -0300

    correcion 1

--- tp5ej12.py
+++ tp5ej12.py
@@ -3,22 +3,25 @@
 # 12. Comparación de listas
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-""""Escribir una función que que determine retornando True si un par de listas contienen los mismos elementos que
+def comparacion_listas(lista1, lista2):
+    """Escribir una función que que determine retornando True si un par de listas contienen los mismos elementos que
 pueden estar estén ubicados en diferentes posiciones."""
+
+    if len(lista1) > len(lista2):
+        for elemento in lista2:
+            comparacion = elemento in lista1
+    else:
+        for elemento in lista1:
+            comparacion = elemento in lista2
+    return comparacion
 
 def prueba():
     """Escribimos dos listas de elementos y las comparamos."""   
 
-lista1=["python","error","comparacion","prueba","buscar","listas","para","uso","contienen"]
-lista2=["python","comparacion","listas"]
-comparacion = [item for item in lista1 if item in lista2]
-
-if (comparacion):
-    """Si encontramos elementos iguales imprimo la lista de ellos."""   
-    print(f"Existen estos elementos: {comparacion} en las 2 listas brindadas.")
-else:
-    """Si NO hay elementos iguales se lo imprimimos"""
-    print("No existe ningun elemento igual en las listas!!!")
+    lista1=["python","error","comparacion","prueba","buscar","listas","para","uso","contienen"]
+    lista2=["python", "uso"]
+    comparacion = comparacion_listas(lista1, lista2)
+    print('tienen los mismos elementos' if comparacion else 'no tienen los mismos elementos')
 
 if __name__ == "__main__":
     prueba()
